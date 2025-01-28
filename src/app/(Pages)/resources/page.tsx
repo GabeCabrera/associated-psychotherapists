427517ff49f4ad2bf66a5d0622057e3169877e17
Gabe Cabrera <gabrielmscabrera@gmail.com>
2025-01-27 21:49:55 -0700

    cleaned up code

--- src/app/(Pages)/resources/page.tsx
+++ src/app/(Pages)/resources/page.tsx
@@ -102,7 +102,7 @@ export default function Resources() {
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
-                        pt: { xs: 5, sm: 5, md: 5, lg: 0  },
+                        pt: { xs: 5, sm: 5, md: 5, lg: 0 },
                         pb: { xs: 5, sm: 5, md: 0, lg: 0 },
                     }}
                 >
@@ -123,19 +123,61 @@ export default function Resources() {
                                 United States
                             </Typography>
                         </Container>
-                        <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
-                            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'baseline', mb: 1 }}>
-                                <h4>EMERGENCY:</h4>  <h4 style={{ color: 'red'}} >&nbsp; 911 </h4>
+                        <Container
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'left'
+                            }}>
+                            <Box sx={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                alignItems: 'baseline',
+                                mb: 1
+                            }}>
+                                <h4>EMERGENCY:</h4>
+                                <h4 style={{ color: 'red' }} >
+                                    &nbsp; 911
+                                </h4>
                             </Box>
-
                             {resourceData.map((data) => (
-                                <Box key={data.id} sx={{ display: 'flex', flexDirection: 'row', alignItems: 'baseline', mb: 1, textWrap: 'noWrap' }}>
-                                    <h4>{data.title}:</h4>  <h4 style={{ color: '#6DA2D6'}} >&nbsp;{data.phoneNumber ? data.phoneNumber : ''} {data.website ? <a style={{ color: '#424242', fontWeight: 'normal', textDecoration: 'underline' }} href={'https://www.' + data.website}>{data.website}</a> : ''}</h4>
+                                <Box key={data.id} sx={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'baseline',
+                                    mb: 1,
+                                    textWrap: 'noWrap'
+                                }}>
+                                    <h4>{data.title}:</h4>
+                                    <h4 style={{ color: '#6DA2D6' }} >
+                                        &nbsp;{data.phoneNumber ? data.phoneNumber : ''},&nbsp;
+                                        {data.website ?
+                                            <a
+                                                style={{
+                                                    color: '#424242',
+                                                    fontWeight: 'normal',
+                                                    textDecoration: 'underline'
+                                                }}
+                                                href={'https://www.' + data.website}
+                                            >
+                                                {data.website}
+                                            </a> : ''}
+                                    </h4>
                                 </Box>
                             ))}
                         </Container>
                     </Card>
-                    <Typography variant="subtitle1" component="div" gutterBottom sx={{ fontFamily: 'Roboto', fontWeight: 300, fontSize: '0.8rem', mt: 3, maxWidth: '75%' }}>
+                    <Typography 
+                        variant="subtitle1" 
+                        component="div" 
+                        gutterBottom 
+                        sx={{ 
+                            fontFamily: 'Roboto', 
+                            fontWeight: 300, 
+                            fontSize: '0.8rem', 
+                            mt: 3, 
+                            maxWidth: '75%'
+                        }}>
                         Updated on Jan 27th, 2025.
                         Associated Psychotherapy assumes no responsibility or liability for the professional ability, reputation, or quality of services provided by the entities or individuals listed above. Inclusion on this list does not constitute an endorsement by Associated Psychotherapy. The order does not imply any ranking or evaluation. Associated Psychotherapy cannot vouch for the contact information's accuracy; If you discover any inaccuracies in the contact information, please email us at contact@associatedpsych.com so we can update it.
                     </Typography>
